ec449dd7aa6a797ad337806b0f2d48cc60bdf7db
woojun <22@>
2025-10-17 12:43:31 +0900

    요구사항 4번

---
+++ DataBase/LibDB.java
@@ -0,0 +1,27 @@
+package DataBase;
+import java.util.*;
+/**
+ * 여기에 LibDB 클래스 설명을 작성하십시오.
+ * 
+ * @author (2022320035_배우준) 
+ * @version (25.10.17)
+ */
+public class LibDB<T extends DB_Element>
+{
+    //ArrayList를 참조한 Pirvate 형태의 변수 db정의 
+    private ArrayList<T> db;
+    
+    
+    public void addElement(T element){
+        
+    }
+    
+    public String findElement(){
+        //이터레이터로 반복작업 써야함
+        
+    }
+    
+    public void printAllElements(){
+        //for each문 사용하기
+    }
+}
