85d051d0ddc255e6feda076faa7d3b4eac9e54a5
woojun <22@>
2025-10-17 12:52:02 +0900

    visson2

--- DataBase/LibDB.java
+++ DataBase/LibDB.java
@@ -1,4 +1,5 @@
 package DataBase;
+import Myclass.*;
 import java.util.*;
 /**
  * 여기에 LibDB 클래스 설명을 작성하십시오.
@@ -11,14 +12,14 @@ public class LibDB<T extends DB_Element>
     //ArrayList를 참조한 Pirvate 형태의 변수 db정의 
     private ArrayList<T> db;
     
-    
+    // 리턴값 없음, db 요소를 추가하는 파라메터 1개
     public void addElement(T element){
         
     }
     
     public String findElement(){
         //이터레이터로 반복작업 써야함
-        
+        return()
     }
     
     public void printAllElements(){
