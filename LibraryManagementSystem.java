d06616792c3ec6993fa8157ddfacea853f94e15c
woojun <22@>
2025-10-17 13:09:01 +0900

    virson3

--- LibraryManagementSystem.java
+++ LibraryManagementSystem.java
@@ -1,10 +1,34 @@
 /**
  * 여기에 LibraryManagementSystem 클래스 설명을 작성하십시오.
  * 
- * @author (작성자 이름) 
- * @version (버전번호나 날짜)
+ * @author (2022320035_배우준) 
+ * @version (25.10.17)
  */
 public class LibraryManagementSystem
 {
-    
+     public void main(){
+         
+     }
+     
+     public void printDB(){
+         
+         
+     }
+     
+     public void printLoanList(){
+         
+         
+     }
+     
+     public LibDB <Book> setBookDB(bookfile String){
+         
+         //책 파일에서 데이터를 읽어들여서 책 객체를 생성 후 책DB에 저장하는 메소드
+     }
+     
+     public LibDB<User> setUserDB(userFile String){
+         
+         //유저 파일의 데이터를 읽어들여서 유저 객체를 생성 후 유저DB에 저장하는 메소드
+         //내부의 반복작업은 일반적인 for문을 사용
+
+     }
 }
