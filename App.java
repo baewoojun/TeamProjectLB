13f1ff4995f3314327868965b05e4ac25c816c46
woojun <22@>
2025-10-17 11:35:15 +0900

    test

---
+++ App.java
@@ -0,0 +1,14 @@
+
+/**
+ * 여기에 App 클래스 설명을 작성하십시오.
+ * 
+ * @author (2022320035_배우준) 
+ * @version (25/10/17)
+ */
+public class App
+{
+    public static void main (String[] args){
+        
+        
+    }
+}
--- tset2.java
+++ tset2.java
@@ -1,5 +1,6 @@
 public class tset2
 //테스트용  
+//테스트 2
 {
     public static void main(String[] args) {
         System.out.println("Hello Team!");
